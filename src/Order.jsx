6fc88cdc5f64a8e48aeba36d73e81a1367c6016e
Ayu <aayuwulandarii03@gmail.com>
2023-06-26 16:15:18 +0700

    add button state

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,9 +1,22 @@
-const order = () => {
+import { useState } from "react"
+
+const Order = () => {
+    const [Count, setCount] = useState(0) 
+
+    console.log(Count)
     return (
         <>
-        INI HALAMAN ORDER
+        <h1>INI HALAMAN ORDER</h1>
+
+        <button
+            onClick = {() => {
+                console.log('test')
+            }}
+            >
+            Count : {Count}
+        </button>
         </>
     )
 }
 
-export default order
+export default Order
--- src/Routers/Router.jsx
+++ src/Routers/Router.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes , Route} from "react-router-dom";
+import AboutUs from "../AboutUs";
 import App from "../App";
 import Menu from "../Menu";
+import Order from "../Order";
 
 export default function Routers() {
     return (
@@ -8,6 +10,9 @@ export default function Routers() {
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/menu" element={<Menu />} />
+                <Route path="/order" element={<Order />} />
+                <Route path="/aboutus" element={<AboutUs/>} />
+
             </Routes>
         </BrowserRouter>
     )
